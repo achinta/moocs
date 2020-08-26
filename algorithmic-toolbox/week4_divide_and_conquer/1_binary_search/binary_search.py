f9bff1df4efabfceeafadcb9b1c5411cf9538af7
Ajay Chinta <ajay.chinta@gmail.com>
2020-08-26 08:05:22 +0530

    binary search works

--- algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,11 +1,38 @@
 # Uses python3
 import sys
+import logging
 
 def binary_search(a, x):
     left, right = 0, len(a)
-    mid = left + (right-left)/2
+    if left == right:
+        return -1
+    mid = left + (right-left)//2
+    # print(f'\n searching for {x} in {a}. mid is {mid} with mid-value - {a[mid]}')
     if a[mid] == x:
         return mid
+    if a[mid] > x:
+        return binary_search(a[left:mid], x)
+    else:
+        result = binary_search(a[mid+1:right],x)
+        if result != -1:
+            result += mid + 1
+        return result
+
+def binary_search2(a, x):
+    low, high = 0, (len(a) - 1)
+    while low <= high:
+        mid = low + (high-low)//2
+
+        if a[mid] == x:
+            return mid
+        elif a[mid] > x:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    if low > high:
+        return -1
+
 
 
 def linear_search(a, x):
@@ -22,4 +49,5 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search2(a, x), end=' ')
+        # binary_search(a, x)
